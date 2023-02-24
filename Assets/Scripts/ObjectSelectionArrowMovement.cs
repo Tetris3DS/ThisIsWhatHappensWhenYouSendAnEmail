935b596f3d1ece8239f4687dfe61ece25b4d556b
Tetris3DS <schnetzer.ben@gmail.com>
2023-02-24 00:14:22 -0500

    Continued Testing of SelectableObjects Control
    Tried making the red square in TestScene move when in the rigidbody of the WandRange

--- Assets/Scripts/ObjectSelectionArrowMovement.cs
+++ Assets/Scripts/ObjectSelectionArrowMovement.cs
@@ -1,19 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
+
 
 public class ObjectSelectionArrowMovement : MonoBehaviour
 {
     public float speed = 10f;
+ //  public Rigidbody2D m_Rigidbody;
+   // float m_Speed;
 
-    public void OnTriggerEnter(Collider info)
+    void Start()
     {
-         if(info.gameObject.tag == "WandRange")
+   // m_Speed = 10f;
+   // m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+         if(collider.gameObject.tag == "WandRange")
          {
               if (Input.GetKey(KeyCode.RightArrow))
               {
                    transform.position += Vector3.right * speed * Time.deltaTime;
+
               }
+              if (Input.GetKey(KeyCode.UpArrow))
+              {
+                   transform.position += Vector3.up * Time.deltaTime;
+              }
+
          }
     }
 }
