ff44719e6c65831689f0bbaf184334f27330c273
Tetris3DS <schnetzer.ben@gmail.com>
2023-04-10 13:57:11 -0400

    Created Levels 2-4 and Added Scene Transitions
    Like it says

--- Assets/FlagTouch.cs
+++ Assets/FlagTouch.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FlagTouch : MonoBehaviour
 {
@@ -17,11 +18,13 @@ public class FlagTouch : MonoBehaviour
        
     }
 
-    void OnCollisionEnter2D(Collision2D col)
+    IEnumerator OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Player")
         {
             flagnoise.Play();
+            yield return new WaitForSeconds(5);
+            SceneManager.LoadScene("Level1Updated");
         }
     }
 }
